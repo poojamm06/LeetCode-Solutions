ff32e0872c45fab0ab8394dea56223b145958f88
pooja MM <poojamm0613@gmail.com>
2026-01-07 08:21:34 +0000

    Sync LeetCode submission Runtime - 1 ms (99.06%), Memory - 42.7 MB (15.27%)

---
+++ leetcode-solutions/0263-ugly-number/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<1) return false;
+        while(n>1){
+            if(n%2==0){
+                n/=2;
+                continue;
+            }
+            else if(n%3==0){
+                n/=3;
+                continue;
+            }
+            else if(n%5==0){
+                n/=5;
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
