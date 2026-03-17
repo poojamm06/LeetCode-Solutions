7e6080afbf9a0ef62899652314bafd677aad2ac9
pooja MM <poojamm0613@gmail.com>
2026-03-17 04:49:50 +0000

    Sync LeetCode submission Runtime - 1 ms (99.95%), Memory - 77 MB (83.59%)

---
+++ leetcode-solutions/0053-maximum-subarray/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr=0;
+        int max=nums[0];
+        for(int i:nums)
+        {
+            curr+=i;
+            if(curr>max){
+                max=curr;
+            }
+            if(curr<0){
+                curr=0;
+            }
+        }
+        return max;
+    }
+}
