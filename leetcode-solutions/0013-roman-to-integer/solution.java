85020f83c7d5ee22ef7b23b2a0d8756b60bf421c
pooja MM <poojamm0613@gmail.com>
2026-03-26 04:18:24 +0000

    Sync LeetCode submission Runtime - 3 ms (77.75%), Memory - 46.1 MB (94.01%)

--- leetcode-solutions/0013-roman-to-integer/solution.java
+++ leetcode-solutions/0013-roman-to-integer/solution.java
@@ -1,30 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        int total=0;
-        int prev=0;
+        int tot=0;
         for(int i=s.length()-1;i>=0;i--){
-            int value=getValue(s.charAt(i));
-            if(value<prev){
-                total-=value;
-            }
-            else{
-                total+=value;
-            }
-            prev = value;
+          int  value=getvalue(s.charAt(i));
+          if(i<s.length()-1 && value<getvalue(s.charAt(i+1))){
+            tot-=value;
+          }
+          else{
+            tot+=value;
+          }
+
+                
+            
         }
-        return total;
+        return tot;
     }
-        private int getValue(char ch){
-            switch(ch){
-                case 'I': return 1;
-                case 'V': return 5;
-                case 'X': return 10;
-                case 'L': return 50;
-                case 'C': return 100;
-                case 'D': return 500;
-                case 'M': return 1000;
-                default: return 0;
-            }
-        }
-    
+
+private int getvalue(char ch){
+    switch(ch){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
 }
