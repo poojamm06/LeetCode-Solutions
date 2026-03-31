90a4dc1a1f2fb2a68ea17c7a5239a78595e12b4a
pooja MM <poojamm0613@gmail.com>
2026-03-31 05:18:35 +0000

    Sync LeetCode submission Runtime - 1 ms (61.20%), Memory - 43.2 MB (67.88%)

--- leetcode-solutions/0014-longest-common-prefix/solution.java
+++ leetcode-solutions/0014-longest-common-prefix/solution.java
@@ -1,12 +1,23 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String prefix = strs[0];
-        for(int i=1;i<strs.length;i++){
-            while(!strs[i].startsWith(prefix)){
-                prefix=prefix.substring(0,prefix.length()-1);
-                if(prefix.isEmpty()) return "";
+        StringBuilder sb=new StringBuilder();
+        if(strs.length==0) {return "";}
+        String first=strs[0];
+        for(int i=0;i<first.length();i++){
+            int count=0;
+            char ch=first.charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
+                    return sb.toString();}
+                   
+                    
+                   
+                }
+                sb.append(ch);
             }
-        }
-        return prefix;
+
+        
+        return sb.toString();
+
     }
 }
