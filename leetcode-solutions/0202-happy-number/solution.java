db5dd629d7799d8efea0aab2e374910908f0e7d9
pooja MM <poojamm0613@gmail.com>
2026-04-02 07:38:17 +0000

    Sync LeetCode submission Runtime - 1 ms (77.64%), Memory - 42.5 MB (51.44%)

---
+++ leetcode-solutions/0202-happy-number/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1 && n!=4){
+            n=getSum(n);
+        }
+        return n==1;
+    }  
+
+
+    public int   getSum(int n){
+        int sum=0;
+        while(n>0){
+            sum+=((n%10)*(n%10));
+            n/=10;
+        }
+        return sum;
+    }                                                                                                                                                           
+}
