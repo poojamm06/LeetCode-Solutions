8436aab254e8761f319f4bea59a301bf34d96eb3
pooja MM <poojamm0613@gmail.com>
2026-07-20 11:26:20 +0000

    Sync LeetCode submission Runtime - 29 ms (32.97%), Memory - 82.7 MB (78.59%)

---
+++ leetcode-solutions/0948-sort-an-array/solution.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergesort(nums,0,nums.length-1);
+        return nums ;
+    }
+    static void merge(int[] nums,int left,int mid,int right){
+        int n1=mid-left+1;
+        int n2=right-mid;
+        int[] L=new int[n1];
+        int[] R=new int[n2];
+        
+        for(int i=0;i<n1;i++){
+            L[i]=nums[left+i];
+        
+        }
+        for(int j=0;j<n2;j++){
+            R[j]=nums[mid+1+j];
+    
+        }
+        int i=0,j=0;
+        int k=left;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            nums[k]=L[i];
+            i++;
+        
+        }
+        else{
+            nums[k]=R[j];
+            j++;        }
+            k++;
+         
+    }
+    while(i<n1){
+        nums[k]=L[i];
+        k++;
+        i++;
+    }
+    while(j<n2){
+        nums[k]=R[j];
+        k++;
+        j++;
+    }   
+            }
+    static void mergesort(int[] nums,int left,int right){
+        if(left<right){
+            int mid=left+(right-left)/2;
+            mergesort(nums,left,mid);
+            mergesort(nums,mid+1,right);
+            merge(nums,left,mid,right);
+        }
+        
+    }
+}
