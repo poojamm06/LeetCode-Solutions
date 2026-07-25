0e91be64806a65609352b8cbdf0a4b4cf2ce489c
pooja MM <poojamm0613@gmail.com>
2026-07-25 16:28:34 +0000

    Sync LeetCode submission Runtime - 2 ms (95.56%), Memory - 48.2 MB (91.80%)

--- leetcode-solutions/0167-two-sum-ii---input-array-is-sorted/solution.java
+++ leetcode-solutions/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-         int l=0;
-         int r=numbers.length-1;
-         while(l<r){
-            int current_sum=numbers[l]+numbers[r];
-            if(current_sum==target){
-                return new int[]{l+1,r+1};
-            }
-            else if(current_sum>target){
-                r--;
-            }
-            else if (current_sum<target){
-                l++;
-            }
-         }
-         return new int[]{};
+         int n=numbers.length;
+        int i=0,j=n-1;
+       
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+               return new int[]{i+1,j+1};}
+               else if (sum>target){
+                  j--;
+               }
+               else if(sum<target){
+                i++;
+               }
+            
+        }
+        return new int[]{};
     }
 }
